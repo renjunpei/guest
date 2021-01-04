b3adb491d5e966ccabfc164f7134fa6f288e4d62
renjunpei <renjunpei@163.com>
2021-01-04 18:59:28 +0800

    增加用户登录页面判断和增加登录用户的校验

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponsePermanentRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 
@@ -16,18 +18,26 @@ def login_action(request):
         if not all([username, password]):
             # 数据不完整
             return render(request, 'index.html', {'error': '数据不完整'})
-        if username == "admin" and password == "admin123":
-            # return HttpResponse("login success")
-            # return render(request,"event_mange.html")
-            # return HttpResponsePermanentRedirect("/event_mange/")   # (HttpResponsePermanentRedirect)路径重定向，从而路径指向event_mange
-            reponse = HttpResponsePermanentRedirect("/event_mange/")
-            # reponse.set_cookie("user", username, 3600)    # 添加cookie
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
             request.session["user"] = username
+            reponse = HttpResponsePermanentRedirect("/event_mange/")
             return reponse
+
+        # # if username == "admin" and password == "admin123":
+        #     # return HttpResponse("login success")
+        #     # return render(request,"event_mange.html")
+        #     # return HttpResponsePermanentRedirect("/event_mange/")   # (HttpResponsePermanentRedirect)路径重定向，从而路径指向event_mange
+        #     reponse = HttpResponsePermanentRedirect("/event_mange/")
+        #     # reponse.set_cookie("user", username, 3600)    # 添加cookie
+        #     request.session["user"] = username
+        #     return reponse
         else:
             return render(request,"index.html",{"error":"用户名或密码错误"})
 
 # 登录成功页面
+@login_required
 def event_mange(request):
     # username = request.COOKIES.get("user","")
     username = request.session.get("user", "")
